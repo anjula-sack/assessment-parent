0d2761251a1a92621a8dabfaad980c62b12176d2
anjula-sack <anjulashanaka@gmail.com>
2025-12-01 10:08:25 +0530

    Fix school name issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -646,6 +646,7 @@ function ParentQuestionnaire() {
                     setFormData((prev) => ({
                       ...prev,
                       school: e.target.value,
+                      isNewSchool: false,
                     }))
                   }
                   className="w-full px-3 py-2 border border-gray-300 rounded-md 
